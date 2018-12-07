7e9f50199e0bd1baf9cd517d32100b07052c4198
Theodore95 <jeroenvanheiningen95@gmail.com>
2018-12-07 12:23:57 +0100

    Nutteloze commit

--- PracticeWorkspace/Chapter4 Example2/app.js
+++ PracticeWorkspace/Chapter4 Example2/app.js
@@ -22,11 +22,33 @@ var main = function () {
             addCommentFromInputBox();
         }      
     });
-    
-    var $newUL = $("<ul>");
-    var $newParagraphElement = $("<p>");
-    $newParagraphElement.text("this is a paragraph");
-    
 
+    var $content = $("<div>Hello World!</div>").hide();
+    var $moreContent = $("<div>Goodbye World!</div>").hide();
+    $("body").append($content);
+    $("footer p").fadeOut(1000, function() {
+        $("footer p").remove();
+    });
+
+
+    
 };
 $(document).ready(main);
+
+/*var $newUL = $("<ul>");
+    var $newParagraphElement = $("<p>");
+    $newParagraphElement.text("this is a paragraph");
+    $("footer").append($newParagraphElement);
+    $("main").append($newUL);
+
+    //we can chain our creation and call to add the text
+    var $listItemOne = $("<li>").text("this is the first list item");
+    var $listItemTwo = $("<li>").text("second list item");
+    var $listItemThree = $("<li>").text("OMG third list item");
+
+    //now we'll append these elements to the ul element we created earlier
+    $newUL.append($listItemOne);
+    $newUL.append($listItemTwo);
+    $newUL.append($listItemThree);
+    
+    $("footer p").fadeOut();*/
