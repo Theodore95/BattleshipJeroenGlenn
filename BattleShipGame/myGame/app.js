c405cc04af2d5126feab512210458dde7dfd79c8
Theodore_95 <jeroenvanheiningen95@gmail.com>
2018-12-08 16:51:31 +0100

    First real work

---
+++ BattleShipGame/myGame/app.js
@@ -0,0 +1,16 @@
+/*var express = require("express");
+var http = require("http");
+
+var port = process.argv[2];
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+http.createServer(app).listen(port);*/
+var express = require("express");
+var http = require("http");
+
+var port = process.argv[2];
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+http.createServer(app).listen(port);
